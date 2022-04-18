b1871f0c58508fa8be3efcf4bbb14213b3069667
angelique-df <faye.angelique.d@gmail.com>
2022-04-18 22:24:55 +0000

    Create SingleJob.js

---
+++ src/components/SingleJob.js
@@ -0,0 +1,74 @@
+import JobDescriptionTerm from "./JobDescriptionTerm";
+
+const SingleJob = ({
+	job: {
+		company,
+		position,
+		postedAt,
+		contract,
+		role,
+		location,
+		level,
+		logo,
+		languages,
+		tools,
+		featured: isFeatured,
+		new: isNew,
+	},
+	addFilter,
+}) => {
+	/**
+	 * Displays "Featured" or "New!" if
+	 */
+	const TagListElement = ({ tag, content, customClass }) => {
+		if (tag) {
+			return <li className={`tag ${customClass}`}>{content}</li>;
+		}
+	};
+	return (
+		<>
+			{" "}
+			<article className={isFeatured ? "featured-job" : null}>
+				<header>
+					<img className="company-logo" src={logo} alt={`Logo of ${company}`} />
+					<div>
+						{/* ".job-position" and ".job-subtitle" have been reversed in the CSS */}
+						<h2 className="job-position">{position}</h2>
+						<div className="job-subtitle">
+							<p className="company-name">{company}</p>
+
+							{/* Conditionnally render the "NEW" and "Featured!" tag TagListElement is not rendered if its tag is === false */}
+							{isNew || isFeatured ? (
+								<ul>
+									<TagListElement
+										tag={isNew}
+										content="New!"
+										customClass="new-tag"
+									/>
+									<TagListElement
+										tag={isFeatured}
+										content="featured"
+										customClass="featured-tag"
+									/>
+								</ul>
+							) : null}
+						</div>
+						<ul className="job-meta">
+							<li className="publication-date">{postedAt}</li>
+							<li className="contract">{contract}</li>
+							<li className="location">{location}</li>
+						</ul>
+					</div>
+				</header>
+
+				<ul className="description">
+					{[role, level, ...languages, ...tools].map((term) => (
+						<JobDescriptionTerm key={term} term={term} addFilter={addFilter} />
+					))}
+				</ul>
+			</article>
+		</>
+	);
+};
+
+export default SingleJob;
